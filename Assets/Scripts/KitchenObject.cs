1625ec075d78a973168a5aef18f8ce16da442b0f
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2023-04-01 18:07:40 +0200

    Initial commit

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -30,4 +30,24 @@ public class KitchenObject : MonoBehaviour
     }
 
     public IKitchenObjectParent GetKitchenObjectParent() { return kitchenObjectParent; }
+
+
+    public void DestroySelf()
+    {
+        kitchenObjectParent.ClearKitchenObject();
+
+        Destroy(gameObject);
+    }
+
+
+    public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
+    {
+        Transform KitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+        KitchenObject kitchenObject = KitchenObjectTransform.GetComponent<KitchenObject>();
+        
+        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
+
+        return kitchenObject;
+    }
+
 }
