3638e541927ee7cdf0d3bf1ce12553b6733506ba
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2023-04-14 03:36:06 +0200

    Initial commit

--- Assets/Scripts/CarMotion.cs
+++ Assets/Scripts/CarMotion.cs
@@ -7,31 +7,49 @@ public class CarMotion : MonoBehaviour
     [SerializeField] private float speed = 10.0f;
     [SerializeField] private float stopPosition = -6f;
     [SerializeField] private float destroyPosition = 36f;
+    [SerializeField] private float nextSpawnTime = 0.0f;
+    [SerializeField] private float spawnInterval = 1.0f;
+
+
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private Vector3 spawnPosition;
+
     private bool deliverySuccess = false;
     private void Start()
     {
         DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
     }
 
     private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e)
     {
         deliverySuccess = true;
         speed = 10.0f;
+        GameObject newObject = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+        newObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
     }
 
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        objectToSpawn.transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
-        if(transform.position.z >= stopPosition && !deliverySuccess )
+        if(objectToSpawn.transform.position.z >= stopPosition && !deliverySuccess )
         {
             speed = 0.0f;
         }
 
-        if(transform.position.z >= destroyPosition)
+        if(objectToSpawn.transform.position.z >= destroyPosition)
         {
             Destroy(gameObject);
+            deliverySuccess = false;
         }
+        if (Time.time >= nextSpawnTime)
+        {
+            nextSpawnTime = Time.time + spawnInterval;
+
+
+        }
+
     }
 
 
