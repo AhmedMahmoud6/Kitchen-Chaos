7d136fd88b12858c601733be974658c8cd1f4120
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2023-04-14 04:44:39 +0200

    Initial commit

--- Assets/Scripts/CarMotion.cs
+++ Assets/Scripts/CarMotion.cs
@@ -7,47 +7,40 @@ public class CarMotion : MonoBehaviour
     [SerializeField] private float speed = 10.0f;
     [SerializeField] private float stopPosition = -6f;
     [SerializeField] private float destroyPosition = 36f;
-    [SerializeField] private float nextSpawnTime = 0.0f;
-    [SerializeField] private float spawnInterval = 1.0f;
-
-
     [SerializeField] private GameObject objectToSpawn;
     [SerializeField] private Vector3 spawnPosition;
 
     private bool deliverySuccess = false;
+    private GameObject spawnedObject; // reference to the instantiated object
     private void Start()
     {
         DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
-        Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+        spawnedObject = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
     }
 
     private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e)
     {
         deliverySuccess = true;
         speed = 10.0f;
-        GameObject newObject = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
-        newObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
     }
 
     void Update()
     {
-        objectToSpawn.transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
-        if(objectToSpawn.transform.position.z >= stopPosition && !deliverySuccess )
+        if(spawnedObject.transform.position.z >= stopPosition && !deliverySuccess )
         {
             speed = 0.0f;
         }
 
-        if(objectToSpawn.transform.position.z >= destroyPosition)
+        if(spawnedObject.transform.position.z >= destroyPosition)
         {
-            Destroy(gameObject);
+            Destroy(spawnedObject.gameObject);
             deliverySuccess = false;
+            spawnedObject = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
         }
-        if (Time.time >= nextSpawnTime)
+        if (spawnedObject != null) // if the prefab is instantiated
         {
-            nextSpawnTime = Time.time + spawnInterval;
-
-
+            spawnedObject.transform.Translate(Vector3.forward * speed * Time.deltaTime); // move the prefab forward
         }
 
     }
